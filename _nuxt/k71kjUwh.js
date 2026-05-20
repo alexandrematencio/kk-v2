8291daf4a639e72cb57f64cde0cf796efaf3fca2
Alexandre Matencio <amatencio@pm.me>
2026-05-20 11:31:58 +0200

    deploy

---
+++ _nuxt/k71kjUwh.js
@@ -0,0 +1,1 @@
+import{_ as r,o as s,c as o,I as t}from"./B8k6v2Ic.js";const n={};function c(e,a){return s(),o("tr",null,[t(e.$slots,"default")])}const l=Object.assign(r(n,[["render",c]]),{__name:"ProseTr"});export{l as default};
